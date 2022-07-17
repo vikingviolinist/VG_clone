24076ad3fff722d8e7163a0f553fc80328f55f2b
vikingviolinist <misopetrik@gmail.com>
2022-07-17 10:45:10 +0200

    ♻️  Refactor to reducer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer } from 'react';
 
 import logo from './assets/logo.png';
 
 import Loader from './components/Loader';
 import Row from './components/Row';
 
-const App = () => {
-  const [rows, setRows] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
+const initialState = {
+  loading: true,
+  result: null,
+  error: null,
+};
+
+const reducer = (state, action) => {
+  console.log(action.payload);
+  switch (action.type) {
+    case 'LOADING':
+      return initialState;
+    case 'RESPONSE_COMPLETE':
+      return {
+        loading: false,
+        result: action.payload.data,
+        error: null,
+      };
+    case 'ERROR':
+      return {
+        loading: false,
+        result: null,
+        error: action.payload.error,
+      };
+    default:
+      return state;
+  }
+};
+
+const useArticles = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
-    setLoading(true);
+    dispatch({ type: 'LOADING' });
 
     fetch('https://storage.googleapis.com/aller-structure-task/test_data.json')
       .then((res) => res.json())
-      .then(([data]) => setRows(data))
-      .catch((error) => setError(error))
-      .finally(setLoading(false));
+      .then(([data]) => dispatch({ type: 'RESPONSE_COMPLETE', payload: { data } }))
+      .catch((error) => dispatch({ type: 'ERROR', payload: { error } }));
   }, []);
+
+  console.log(state);
+  return [state.result, state.loading, state.error];
+};
+const App = () => {
+  const [rows, loading, error] = useArticles();
 
   if (error) return <h1>There was an error fetching articles</h1>;
 
